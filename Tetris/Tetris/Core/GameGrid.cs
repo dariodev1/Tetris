2191e87bc1f79beb2c7a1d0ece42c794b42ab452
dariodev1 <dariuszszpunar@outlook.com>
2023-02-09 21:01:14 +0100

    base class for every type of block + basic game grid class

---
+++ Tetris/Tetris/Core/GameGrid.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.Core
+{
+    public class GameGrid
+    {
+        private readonly int[,] _grid;
+        public int Rows { get; }
+        public int Columns { get; }
+
+        public int this[int row, int column]
+        {
+            get { return _grid[row, column]; }
+            set { _grid[row, column] = value; }
+        }
+
+        public GameGrid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            _grid = new int[rows, columns];
+        }
+
+        public bool IsInside(int row, int column)
+        {
+            return row >= 0 && row < Rows && column >= 0 && column < Columns;
+        }
+
+        public bool IsEmpty(int row, int columns)
+        {
+            return IsInside(row, columns) && _grid[row, columns] == 0;
+        }
+
+        public bool IsRowFull(int row)
+        {
+            for (int col = 0; col < Columns; col++)
+            {
+                if (_grid[row, col] == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsRowEmpty(int row)
+        {
+            for (int col = 0; col < Columns; col++)
+            {
+                if (_grid[row, col] != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void ClearRow(int row)
+        {
+            for (int col = 0; col < Columns; col++)
+            {
+                _grid[row, col] = 0;
+            }
+        }
+
+        private void MoveRowDown(int row, int numRows)
+        {
+            for (int col = 0; col < Columns; col++)
+            {
+                _grid[row + numRows, col] = _grid[row, col];
+                _grid[row, col] = 0;
+            }
+        }
+
+        public int ClearFullRows()
+        {
+            int numberOfClearedRows = 0;
+
+            for (int row = Rows - 1; row >= 0; row--)
+            {
+                if (IsRowFull(row))
+                {
+                    ClearRow(row);
+                    numberOfClearedRows++;
+                }
+                else if (numberOfClearedRows > 0)
+                {
+                    MoveRowDown(row, numberOfClearedRows);
+                }
+            }
+
+            return numberOfClearedRows;
+        }
+    }
+}
