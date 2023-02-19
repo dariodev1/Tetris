54f78f679b66893d8ebe3e5818f5ebae460777c1
dariodev1 <dariuszszpunar@outlook.com>
2023-02-19 22:12:56 +0100

    class which generate new random block added

---
+++ Tetris/Tetris/Core/BlockQueue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.Core
+{
+    public class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new OBlock(),
+            new SBlock(),
+            new LBlock(),
+            new JBlock(),
+            new ZBlock(),
+            new TBlock()
+        };
+
+        private readonly Random random = new Random();
+
+        public Block NextBlock { get; private set; }
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+
+        private Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            } while (block.Id == NextBlock.Id);
+
+            return block;
+        }
+    }
+}
